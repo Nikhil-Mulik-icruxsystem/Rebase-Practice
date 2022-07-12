96f674216bd98ad2a003cd5f2859a4eac4cc12af
Nikhil <nikhil.mulik@icruxsystem.com>
2022-07-12 15:58:47 +0530

    nprandncr.java 1st commit

--- nprandncr.java
+++ nprandncr.java
@@ -4,6 +4,7 @@ public class nprandncr {
 //calculating a factorial of a number
 public static int fact(int num)
 {
+
 int fact=1, i;
 for(i=1; i<=num; i++)
 {
@@ -14,6 +15,7 @@ return fact;
 public static void main(String args[])
 {
 int n, r;
+int npr = fact;
 Scanner scan = new Scanner(System.in);
 System.out.print("Enter Value of n : ");
 n = scan.nextInt();
